78c46ad0bc4e25bf4114c97b95c804f268ba6d90
eric-araujo <eric.araujolima@hotmail.com>
2020-04-27 14:58:02 -0300

    Detectando URL's

--- index.js
+++ index.js
@@ -2,6 +2,34 @@ const http = require('http');
 
 let server = http.createServer((req, res) => {
 
+    switch (req.url) {
+
+        case '/':
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<h1>Olá</h1>');
+
+            break;
+
+        case '/users':
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({
+
+                users: [{
+
+                    name: 'Eric',
+                    email: 'eric.araujolima@hotmail.com',
+                    id:1
+
+                }]
+
+            }));
+
+    }
+
     res.end('OK');
 
 });
