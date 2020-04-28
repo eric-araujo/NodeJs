06252747ce3a8703e298995849f28af4af916cbe
eric-araujo <eric.araujolima@hotmail.com>
2020-04-28 16:33:18 -0300

    Buscando um usuário

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ module.exports = (app) => {
 
                 app.utils.error.send(err, req, res);
 
-            } else { 
+            } else {
 
                 res.status(200).json(users);
 
@@ -29,6 +29,26 @@ module.exports = (app) => {
     route.post((req, res) => {
 
         db.insert(req.body, (err, user) => {
+
+            if (err) {
+
+                app.utils.error.send(err, req, res);
+
+            } else {
+
+                res.status(200).json(user);
+
+            }
+
+        });
+
+    });
+
+    let routeId = app.route('/users/:id');
+
+    routeId.get((req, res) => {
+
+        db.findOne({ _id: req.params.id }).exec((err, user) => {
 
             if (err) {
 
